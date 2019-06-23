42fe10cfd69f337cfe96bba4a4c3e359f9485cf6
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-06-23 11:13:40 +0300

    Add animated page change from Splash

--- lib/widgets/landing_page.dart
+++ lib/widgets/landing_page.dart
@@ -8,11 +8,57 @@ class LandingPage extends StatefulWidget {
 }
 
 class LandingPageState extends State<LandingPage> {
+  final PageController pageController = PageController(initialPage: 0);
+  final int numberOfPages = 2;
+  int currentPage = 0;
+
+  @override
+  void initState() {
+    pageController.addListener(() {
+      int next = pageController.page.round();
+      if (currentPage != next) {
+        setState(() {
+          currentPage = next;
+        });
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SplashScreen(),
+        body: PageView.builder(
+          scrollDirection: Axis.horizontal,
+          controller: pageController,
+          itemCount: numberOfPages,
+          itemBuilder: (context, int currentIndex) {
+            bool active = currentPage == currentIndex;
+            if (currentIndex == 0)
+              return SplashScreen(pageController);
+            else
+              return SafeArea(child: _buildHomePage(active));
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHomePage(bool isActive) {
+    final double topAndBottom = isActive ? 0 : 200;
+    final double leftAndRight = isActive ? 0 : 150;
+
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 2000),
+      curve: Curves.easeOutExpo,
+      margin: EdgeInsets.only(
+          top: topAndBottom,
+          bottom: topAndBottom,
+          left: leftAndRight,
+          right: leftAndRight),
+      child: Container(
+        color: Colors.black,
       ),
     );
   }
--- lib/widgets/splash_screen.dart
+++ lib/widgets/splash_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 import '../assets.dart';
 import './hexagon.dart';
 import './logo_blink.dart';
 
 class SplashScreen extends StatelessWidget {
+  SplashScreen(PageController pageController, {int pageIndex = 1}) {
+    Timer(Duration(seconds: 4), () {
+      pageController.animateToPage(pageIndex,
+          duration: Duration(seconds: 2), curve: Curves.easeOutExpo);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
